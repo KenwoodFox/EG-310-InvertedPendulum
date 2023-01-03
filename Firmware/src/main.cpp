1c5304c7e99918e0b5259821bc09c6cf18ba2250
KenwoodFox <kenwood364@gmail.com>
2023-01-03 00:10:16 -0500

    Add MPU library to firmware

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -6,9 +6,15 @@
 
 // Libs
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 // Headers
 #include "boardPins.h"
+
+// Instantiate devices
+Adafruit_MPU6050 mpu;
 
 void setup()
 {
@@ -16,9 +22,28 @@ void setup()
     delay(200);
     Serial.begin(115200);
     Serial.println(MOTD);
+
+    // Setup devices
+    if (!mpu.begin())
+    {
+        while (1)
+        {
+            delay(10);
+            // This would be a good place to flash an error light
+        }
+    }
+    Serial.println("MPU6050 initalized.");
+
+    // Configure devices
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);    // Sets the MPU range
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ); // Sets the loop frequency
 }
 
 void loop()
 {
     delay(2000);
+
+    // Get the values now, this is just for demo, the real device needs to be much faster. So much faster infact we may just need to make our own lib but, we'll see
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 }
