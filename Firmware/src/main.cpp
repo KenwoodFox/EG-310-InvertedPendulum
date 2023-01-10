1143312c5fe0e509941215b7336b85fa47cfc39a
KenwoodFox <kenwood364@gmail.com>
2023-01-09 23:03:20 -0500

    temp firmware

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -5,17 +5,20 @@
  */
 
 //  This option forces Encoder.h to use actual assembly code to squeeze extra performance from the system
-#define ENCODER_OPTIMIZE_INTERRUPTS
+// #define ENCODER_OPTIMIZE_INTERRUPTS
 
 // Libs
 #include <Arduino.h>
-#include <Encoder.h>
+// #include <Encoder.h>
+#include <TFT.h>
+#include <SPI.h>
 
 // Headers
 #include "boardPins.h"
 
 // Instantiate objects
-Encoder pendulumEncoder(ENC_A, ENC_B);
+// Encoder pendulumEncoder(ENC_A, ENC_B);
+TFT disp = TFT(TFT_CS, TFT_A0, TFT_RESET);
 
 void setup()
 {
@@ -24,23 +27,31 @@ void setup()
     Serial.begin(115200);
     Serial.println(MOTD);
 
-    // Configure Pins
-    pinMode(STAT_LED, OUTPUT);
-    pinMode(ENABLE, OUTPUT);
-    pinMode(STEP, OUTPUT);
-    pinMode(DIR, OUTPUT);
-    pinMode(TFT_RESET, OUTPUT);
-    pinMode(TFT_CS, OUTPUT);
-    pinMode(TFT_A0, OUTPUT);
+    // // Configure Pins
+    // pinMode(STAT_LED, OUTPUT);
+    // pinMode(ENABLE, OUTPUT);
+    // pinMode(STEP, OUTPUT);
+    // pinMode(DIR, OUTPUT);
+    // pinMode(TFT_RESET, OUTPUT);
+    // pinMode(TFT_CS, OUTPUT);
+    // pinMode(TFT_A0, OUTPUT);
 
-    pinMode(PPIN, INPUT);
-    pinMode(IPIN, INPUT);
-    pinMode(DPIN, INPUT);
-    pinMode(STOP_MODE, INPUT);
-    pinMode(START, INPUT);
+    // pinMode(PPIN, INPUT);
+    // pinMode(IPIN, INPUT);
+    // pinMode(DPIN, INPUT);
+    // pinMode(STOP_MODE, INPUT);
+    // pinMode(START, INPUT);
+
+    // Configure TFT
+    disp.begin();
 }
 
 void loop()
 {
-    delay(2000);
+    disp.background(255, 0, 0);
+    Serial.println("Set bkground to red");
+    delay(500);
+    disp.background(0, 255, 0);
+    Serial.println("Set bkground to green");
+    delay(500);
 }
